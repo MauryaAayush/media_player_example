079d61dbe4d158ec08db79da2b5e966ec55565c6
Admin <maayush235@gmail.com>
2024-06-01 10:43:07 +0530

    Radhey Radhey

--- lib/Screens/ Carousel_Slider (lec- 1)/CarouselSliderScreen.dart
+++ lib/Screens/ Carousel_Slider (lec- 1)/CarouselSliderScreen.dart
@@ -1,5 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:media_player_example/Screens/%20Carousel_Slider%20(lec-%201)/providers/carousal_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'model/image&text_Model.dart';
 
@@ -8,43 +10,71 @@ class CarouselScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Carousel Example'),
       ),
       body: Center(
-        child: CarouselSlider(
-          items: imgTextList.map((item) {
-            return Builder(
-              builder: (BuildContext context) {
-                return Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.symmetric(horizontal: 0),
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(10.0),
+        child: CarouselSlider.builder(
+          itemCount: imgTextList.length,
+          itemBuilder: (context, index, realIndex) => Container(
+            width: 500,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.8),
+                    blurRadius: 7,
+                    spreadRadius: 2)
+              ],
+              borderRadius: BorderRadius.circular(50),
+              image: DecorationImage(
+                  image: AssetImage(imgTextList[index].img), fit: BoxFit.cover),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 250,
+                    child: Text(
+                      imgTextList[index].quote,
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
                   ),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.asset(
-                        item,
-                        fit: BoxFit.cover,
-                      )),
-                );
-              },
-            );
-          }).toList(),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30, top: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          imgTextList[index].writer,
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
           options: CarouselOptions(
-            height: 600,
+            onPageChanged: (index, reason) =>
+                Provider.of<CarousalProvider>(context, listen: false)
+                    .updatevalue(index),
+            height: 700,
             autoPlay: true,
+            viewportFraction: 0.9,
             enlargeCenterPage: true,
-            aspectRatio: 16/9,
+            aspectRatio: 16 / 9,
             autoPlayCurve: Curves.linear,
             enableInfiniteScroll: true,
-            autoPlayAnimationDuration: const Duration(milliseconds: 1000),
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
           ),
         ),
       ),
--- lib/Screens/ Carousel_Slider (lec- 1)/model/gradientModel.dart
+++ lib/Screens/ Carousel_Slider (lec- 1)/model/gradientModel.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class GradientModel{
   final Gradient gradient;
   final Color color;
@@ -7,3 +9,65 @@ class GradientModel{
   GradientModel({required this.gradient, required this.color});
 }
 
+List<GradientModel> gradientList = [
+  GradientModel(
+    gradient: LinearGradient(
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        colors: [
+          Color(0xff853824),
+          Color(0xff0B0507),
+          Color(0xff6D3224),
+        ]),
+    color: Color(0xff0B0507),
+  ),
+
+  GradientModel(
+    gradient: LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [
+        Color(0xff123456),
+        Color(0xff654321),
+        Color(0xffabcdef),
+      ],
+    ),
+    color: Color(0xff654321),
+  ),
+  GradientModel(
+    gradient: LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [
+        Color(0xffFF5733),
+        Color(0xff33FF57),
+        Color(0xff3357FF),
+      ],
+    ),
+    color: Color(0xff33FF57),
+  ),
+  GradientModel(
+    gradient: LinearGradient(
+      begin: Alignment.bottomRight,
+      end: Alignment.topLeft,
+      colors: [
+        Color(0xff9C27B0),
+        Color(0xffE91E63),
+        Color(0xffFF9800),
+      ],
+    ),
+    color: Color(0xffE91E63),
+  ),
+  GradientModel(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        Color(0xff00BCD4),
+        Color(0xff009688),
+        Color(0xff8BC34A),
+      ],
+    ),
+    color: Color(0xff009688),
+  ),
+];
