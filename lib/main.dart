08d4678fabcd013cacd5f8ad12520ca059f0598c
Admin <maayush235@gmail.com>
2024-06-02 09:41:50 +0530

    Radhey Radhey

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:media_player_example/Screens/%20Carousel_Slider%20(lec-%201)/providers/carousal_provider.dart';
 import 'package:provider/provider.dart';
-
 import 'Screens/ Carousel_Slider (lec- 1)/CarouselSliderScreen.dart';
 
 void main() {
@@ -18,7 +17,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
