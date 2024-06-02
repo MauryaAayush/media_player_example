787371e020d1a10d7ea7ddafb638be31e07ffaff
Admin <maayush235@gmail.com>
2024-06-02 11:17:57 +0530

    Radhey Radhey

--- lib/Screens/Parallax Effect (lec-1)/views/components/videoCard.dart
+++ lib/Screens/Parallax Effect (lec-1)/views/components/videoCard.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoCard extends StatefulWidget {
-  const VideoCard({super.key,required this.assetPath});
+  const VideoCard({super.key, required this.assetPath});
 
   final String assetPath;
 
@@ -13,29 +13,23 @@ class VideoCard extends StatefulWidget {
 class _VideoCardState extends State<VideoCard> {
   late VideoPlayerController _controller;
 
-
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
     _controller = VideoPlayerController.asset(widget.assetPath);
 
     _controller
-    ..addListener(() => setState(() { }))
-    ..setLooping(true)
-    ..setVolume(0)
-    ..initialize().then((value) => setState(() {}))
-    ..play();
+      ..addListener(() => setState(() {}))
+      ..setLooping(true)
+      ..setVolume(0)
+      ..initialize().then((value) => setState(() {}))
+      ..play();
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(
-          vertical: 32,
-          horizontal: 16
-      ),
+      margin: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
       decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(16),
@@ -43,10 +37,8 @@ class _VideoCardState extends State<VideoCard> {
             BoxShadow(
                 color: Colors.black.withOpacity(0.3),
                 offset: Offset(0, 6),
-                blurRadius: 8
-            )
-          ]
-      ),
+                blurRadius: 8)
+          ]),
     );
   }
 }
